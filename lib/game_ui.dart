76c88fe19369ba1e51b7d2975cc79cde651554a5
Nuno Sousa <nuno.mt.sousa@gmail.com>
2024-09-24 18:18:54 +0100

    Extract logic to main instead of letting it live in the game_ui.

--- lib/game_ui.dart
+++ lib/game_ui.dart
@@ -1,93 +1,53 @@
-import 'dart:async';
-
-import 'package:blocks/game.dart';
-import 'package:blocks/piece.dart';
+import 'package:blocks/main.dart';
 import 'package:flutter/material.dart';
 
-class GameUI extends StatefulWidget {
-  const GameUI({super.key});
+class GameUI extends StatelessWidget {
+  final BoardStateNotifier _gameStateNotifier;
 
-  @override
-  State<GameUI> createState() => _GameUIState();
-}
-
-class _GameUIState extends State<GameUI> {
-  final Game _game = Game.empty(
-      _createPieceProvider(Piece([
-        PieceSprite(['P'])
-      ])),
-      10,
-      15);
+  GameUI(this._gameStateNotifier, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    var boardState = _game.getBoardState();
-    var colCount = boardState.colCount;
-    var rowCount = boardState.rowCount;
+    return ListenableBuilder(
+      listenable: _gameStateNotifier,
+      builder: (context, child) {
+        var boardState = _gameStateNotifier.boardState;
+        var colCount = boardState.colCount;
+        var rowCount = boardState.rowCount;
 
-    List<Widget> rowList = [];
-    for (int row = 0; row < rowCount; row++) {
-      List<Widget> rowPixels = [];
-      for (int col = 0; col < colCount; col++) {
-        Color pixelColor = Colors.grey[900]!;
+        List<Widget> rowList = [];
+        for (int row = 0; row < rowCount; row++) {
+          List<Widget> rowPixels = [];
+          for (int col = 0; col < colCount; col++) {
+            Color pixelColor = Colors.grey[900]!;
 
-        var pixelType = boardState.rows[row][col];
-        if (pixelType != ' ') {
-          pixelColor = Colors.white;
+            var pixelType = boardState.rows[row][col];
+            if (pixelType != ' ') {
+              pixelColor = Colors.white;
+            }
+
+            rowPixels.add(Flexible(
+              child: _Pixel(color: pixelColor),
+            ));
+          }
+          rowList.add(Flexible(
+            child: Row(children: rowPixels),
+          ));
         }
 
-        rowPixels.add(Flexible(
-          child: _Pixel(color: pixelColor),
-        ));
-      }
-      rowList.add(Flexible(
-        child: Row(children: rowPixels),
-      ));
-    }
-
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: colCount / rowCount,
-          child: Column(
-            children: rowList,
+        return Scaffold(
+          backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+          body: Center(
+            child: AspectRatio(
+              aspectRatio: colCount / rowCount,
+              child: Column(
+                children: rowList,
+              ),
+            ),
           ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    startGame();
-  }
-
-  void startGame() {
-    Duration frameRate = const Duration(milliseconds: 100);
-    gameLoop(frameRate);
-  }
-
-  void gameLoop(Duration frameRate) {
-    Timer.periodic(
-      frameRate,
-      (timer) {
-        setState(() {
-          _game.tick();
-          if (_game.getState() == GameState.gameOver) {
-            timer.cancel();
-          }
-        });
+        );
       },
     );
-  }
-
-  static Iterable<Piece> _createPieceProvider(Piece piece) sync* {
-    while (true) {
-      yield piece;
-    }
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,59 @@
+import 'dart:async';
+
+import 'package:blocks/game.dart';
 import 'package:blocks/game_ui.dart';
+import 'package:blocks/piece.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  BoardStateNotifier boardStateNotifier = BoardStateNotifier();
+
+  runApp(MyApp(boardStateNotifier));
+
+  Duration frameRate = const Duration(milliseconds: 100);
+  Timer.periodic(frameRate, (timer) {
+    boardStateNotifier.tick();
+    if (boardStateNotifier.gameState == GameState.gameOver) {
+      timer.cancel();
+    }
+  });
+}
+
+class BoardStateNotifier extends ChangeNotifier {
+  late Game _game;
+  BoardState get boardState => _game.getBoardState();
+  GameState get gameState => _game.getState();
+
+  BoardStateNotifier() {
+    _game = Game.empty(
+        _createPieceProvider(Piece([
+          PieceSprite(['P'])
+        ])),
+        10,
+        15);
+  }
+
+  static Iterable<Piece> _createPieceProvider(Piece piece) sync* {
+    while (true) {
+      yield piece;
+    }
+  }
+
+  void tick() {
+    _game.tick();
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final BoardStateNotifier _boardStateNotifier;
+
+  const MyApp(this._boardStateNotifier, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: GameUI(),
+    return MaterialApp(
+      home: GameUI(_boardStateNotifier),
     );
   }
 }
