27240d7121a4e031d741c0cd0715c5603f2cdf24
Nuno Sousa <nuno.mt.sousa@gmail.com>
2024-10-13 11:14:42 +0100

    Removing dead code.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:blocks/game.dart';
 import 'package:blocks/game_notifier.dart';
 import 'package:blocks/game_pieces.dart';
-import 'package:blocks/game_ui.dart';
 import 'package:blocks/block_game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,7 +10,7 @@ import 'package:flutter/services.dart';
 void main() {
   GameNotifier boardStateNotifier = GameNotifier(createGamePieceProvider());
 
-  runApp(const BlockGame()); //MyApp(boardStateNotifier));
+  runApp(const BlockGame());
 
   Duration frameRate = const Duration(milliseconds: 300);
   Timer.periodic(frameRate, (timer) {
@@ -42,42 +41,4 @@ void main() {
       return false;
     },
   );
-}
-
-class MyApp extends StatelessWidget {
-  final GameNotifier _boardStateNotifier;
-
-  const MyApp(this._boardStateNotifier, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
-      body: GameUI(_boardStateNotifier),
-      bottomNavigationBar: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          IconButton(
-              onPressed: () => _boardStateNotifier.moveLeft(),
-              icon: const Icon(
-                Icons.arrow_left,
-                color: Colors.white,
-              )),
-          IconButton(
-              onPressed: () => _boardStateNotifier.rotate(),
-              icon: const Icon(
-                Icons.rotate_right,
-                color: Colors.white,
-              )),
-          IconButton(
-              onPressed: () => _boardStateNotifier.moveRight(),
-              icon: const Icon(
-                Icons.arrow_right,
-                color: Colors.white,
-              ))
-        ],
-      ),
-    ));
-  }
 }
